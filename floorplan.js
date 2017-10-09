f7c96be478f70340ec414e88d175a4858fd8f0b3
RevK <github@e.gg>
2017-10-09 17:10:18 +0100

    Started floorplan work

---
+++ floorplan.js
@@ -0,0 +1,30 @@
+// Floorplan
+
+var backoff=1;
+var ws=null;
+function wsconnect()
+{
+	//document.getElementById("status").textContent="Connecting";
+	ws = new WebSocket((window.location.protocol=="https:"?"wss://":"ws://")+window.location.host+window.location.pathname);
+	ws.onopen=function()
+	{
+		        //document.getElementById("status").textContent="Connected to "+window.location.hostname;
+		        backoff=1;
+	}
+	ws.onclose=function()
+	{
+		        //document.getElementById("status").textContent="DISCONNECTED";
+		        backoff=backoff*2;
+		        setTimeout(wsconnect,100*backoff);
+	}
+	ws.onerror=function()
+	{
+		        //document.getElementById("status").textContent="Error";
+	}
+	ws.onmessage=function(event)
+	{
+		        o=JSON.parse(event.data);
+	}
+}
+
+window.onload=wsconnect;
