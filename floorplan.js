cf893a5de36bc01d013284a8c95dc84faa6d1d1f
RevK <root@excalibur.bracknell.aa.net.uk>
2017-10-10 09:29:26 +0100

    Some (non saved) dragging

--- floorplan.js
+++ floorplan.js
@@ -2,6 +2,30 @@
 
 var backoff=1;
 var ws=null;
+function allowdrop(e)
+{
+	e.preventDefault();
+}
+function startdrop(e)
+{
+	e.dataTransfer.setData("text", e.target.id);
+	this.ox=e.offsetX;
+	this.oy=e.offsetY;
+}
+function dodrop(e)
+{
+	o=document.getElementById(e.dataTransfer.getData("text"));
+	if(o)
+	{
+		console.log(e);
+		o.style.position="absolute";
+		o.style.left=(e.x-o.ox)+"px";
+		o.style.top=(e.y-o.oy)+"px";
+		// TODO reporting back position to permanently store
+		// TODO allow rotation - how
+		// TODO allow picking icon (drop on icon to use?)
+	}
+}
 function wsconnect()
 {
 	//document.getElementById("status").textContent="Connecting";
@@ -27,6 +51,7 @@ function wsconnect()
 					x.id=d.id;
 					x.className="door";
 					x.draggable=true;
+					x.ondragstart=startdrop;
 					x.ondblclick=function()
 					{
 						var a={door:[{id:this.id}]};
@@ -39,19 +64,38 @@ function wsconnect()
 			});
 		        if(o.input)o.input.forEach(function(i)
 			{
-				x=document.getElementById(i.id);
+				x=document.getElementById("input"+i.id);
 				if(!x)
 				{
 					x=document.createElement("img");
-					x.id=i.id;
+					x.id="input"+i.id;
 					x.className="input";
 					x.draggable=true;
+					x.ondragstart=startdrop;
+					x.title=i.name?i.name:i.id;
 					document.getElementById("floorplan").appendChild(x);
 				}
 				x.src="floorplan-input-"+(i.tamper?"tamper":i.fault?"fault":i.active?"active":"idle")+".svg";
-				x.title=i.name?i.name:i.id;
+			});
+		        if(o.output)o.output.forEach(function(o)
+			{
+				x=document.getElementById("output"+o.id);
+				if(!x)
+				{
+					x=document.createElement("img");
+					x.id="output"+o.id;
+					x.className="output";
+					x.draggable=true;
+					x.ondragstart=startdrop;
+					x.title=o.name?o.name:o.id;
+					document.getElementById("floorplan").appendChild(x);
+				}
+				x.src="floorplan-output-"+(o.active?"active":"idle")+".svg";
 			});
 	}
+	x=document.getElementById("floorplan");
+	x.ondragover=allowdrop;
+	x.ondrop=dodrop;
 }
 
 window.onload=wsconnect;
