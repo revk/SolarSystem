105206342d7f36c3640031af561ec5ffd8ea98e3
RevK <github@e.gg>
2019-04-23 07:38:16 +0100

    Reader code in place

--- Reader/Reader.ino
+++ Reader/Reader.ino
@@ -9,6 +9,13 @@
 // GPIO14 SCK (CLK)
 // GPIO16 SDA (SS)
 
+// TODO
+// HoldTime as a setting
+// More checks on MiFare card validity (key as a setting)
+
+#define HOLDTIME 3000
+#define RELEASETIME 1000
+
 #include <ESP8266RevK.h>
 
 ESP8266RevK revk(__FILE__, "Build: " __DATE__ " " __TIME__);
@@ -39,19 +46,39 @@ void setup()
 void loop()
 {
   revk.loop();
-  long now = millis();
+  long now = (millis() ? : 1); // Allowing for wrap, and using 0 to mean not set
   static long cardcheck = 0;
   if ((int)(cardcheck - now) < 0)
   {
-    cardcheck = now + 10;
-    if (rfid.PICC_IsNewCardPresent())
+    cardcheck = now + 100;
+    static byte id[4] = {};
+    static long first = 0;
+    static long last = 0;
+    static boolean held = false;
+    if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial())
     {
-      if (rfid.PICC_ReadCardSerial())
+      last = now;
+      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+      if (!first || memcmp(id, rfid.uid.uidByte, 4))
       {
-        MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-        revk.pub("stat", "id", "%02X%02X%02X%02X", rfid.uid.uidByte[0], rfid.uid.uidByte[1], rfid.uid.uidByte[2], rfid.uid.uidByte[3]);
-	// TODO report once for press and separately for held
+        if (held)
+          revk.stat("gone", "%02X%02X%02X%02X", id[0], id[1], id[2], id[3]); // Edge case of change card after hold before release time
+        first = now;
+        held = false;
+        memcpy(id, rfid.uid.uidByte, 4);
+        revk.stat("id", "%02X%02X%02X%02X", id[0], id[1], id[2], id[3]);
+      } else if (!held && first && (int)(now - first) > HOLDTIME)
+      {
+        held = true;
+        revk.stat( "held", "%02X%02X%02X%02X", id[0], id[1], id[2], id[3]);
       }
+    } else if (last && (int)(now - last) > RELEASETIME)
+    {
+      if (held)
+        revk.stat("gone", "%02X%02X%02X%02X", id[0], id[1], id[2], id[3]);
+      first = 0;
+      last = 0;
+      held = false;
     }
   }
 }
