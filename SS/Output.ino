1858afb5875997dcba7c72a0f0f78ccca38677fc
RevK <github@e.gg>
2019-05-08 12:35:21 +0100

    Allow output pin GPIO 0

--- SS/Output.ino
+++ SS/Output.ino
@@ -80,7 +80,7 @@ unsigned long outputoverride = 0;
       }
       int p = outputpin[i];
 #ifdef ARDUINO_ESP8266_NODEMCU
-      if (!p) for (p = 1; p < MAX_PIN && !((gpiomap | (1 << 1) | (1 << 3)) & (1 << p)); p++); // Find a pin (skip tx/rx)
+      if (p < 0) for (p = 0; p < MAX_PIN && !((gpiomap | (1 << 1) | (1 << 3)) & (1 << p)); p++); // Find a pin (skip tx/rx)
 #endif
       if (p < 0) for (p = 0; p < MAX_PIN && !(gpiomap & (1 << p)); p++); // Find a pin
       if (p == MAX_PIN || !(gpiomap & (1 << p)))
