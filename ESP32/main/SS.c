fe9fe2818d67aabc70f1c7cb81aecd459d8aa20c
Adrian Kennard <github@e.gg>
2019-08-19 12:03:28 +0300

    Made a start

--- ESP32/main/SS.c
+++ ESP32/main/SS.c
@@ -6,12 +6,6 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
-
 #include "../components/ESP32RevK/revk.h"
 
 #include "door.h"
@@ -21,28 +15,18 @@
 #include "ranger.h"
 #include "keypad.h"
 
+const char *app_setting(const char *tag,const char *value)
+{
+return ""; // Unknown
+}
+
+const char *app_command(const char *tag,const char *value)
+{
+return ""; // Unknown
+}
+
 void app_main()
 {
-    printf("Hello world!\n");
-
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+printf("Main SS app\n");
+revk_init(__FILE__,__DATE__,__TIME__,&app_setting,&app_command);
 }
