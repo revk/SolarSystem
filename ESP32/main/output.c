64ad1605aafd185c157e978f1cb9e90fadd9fced
Adrian Kennard <a@k.gg>
2019-10-02 11:06:27 +0100

    Keep outputs locked

--- ESP32/main/output.c
+++ ESP32/main/output.c
@@ -15,7 +15,7 @@ const char *output_tamper = NULL;
 static uint8_t output[MAXOUTPUT];
 
 static uint64_t output_state = 0;       // Port state
-static uint64_t output_unheld = 0;      // Ports we have unheld (i.e. after being first set)
+static uint64_t output_state_set = 0; // Output has been set
 
 int
 output_active (int p)
@@ -37,23 +37,22 @@ output_set (int p, int v)
    if (v)
    {
       v = 1;
-      if ((output_state & (1ULL << p)) && (output_unheld & (1ULL << p)))
+      if ((output_state & (1ULL << p)) && (output_state_set & (1ULL << p)))
          return;                // No change
       output_state |= (1ULL << p);
    } else
    {
-      if (!(output_state & (1ULL << p)) && (output_unheld & (1ULL << p)))
+      if (!(output_state & (1ULL << p)) && (output_state_set & (1ULL << p)))
          return;                // No change
       output_state &= ~(1ULL << p);
    }
    if (output[p])
-      gpio_set_level (port_mask (output[p]), (output[p] & PORT_INV) ? 1 - v : v);
-   if (!(output_unheld & (1ULL << p)))
    {
-      output_unheld |= (1ULL << p);
-      if (output[p])
-         gpio_hold_dis (port_mask (output[p]));
+      gpio_set_level (port_mask (output[p]), (output[p] & PORT_INV) ? 1 - v : v);
+      gpio_hold_dis (port_mask (output[p]));
+      gpio_hold_en (port_mask (output[p]));
    }
+   output_state_set |= (1ULL << p);
 }
 
 int
@@ -62,7 +61,7 @@ output_get (int p)
    if (p < 1 || p > MAXOUTPUT)
       return -1;
    p--;
-   if (!(output_unheld & (1ULL << p)))
+   if (!(output_state_set & (1ULL << p)))
       return -1;
    if (output_state & (1ULL << p))
       return 1;
@@ -85,13 +84,6 @@ output_command (const char *tag, unsigned int len, const unsigned char *value)
       }
    }
    return NULL;
-   if (!strcmp (tag, "restart"))
-   {                            // Hold ports
-      int i;
-      for (i = 0; i < MAXOUTPUT; i++)
-         if (output[i])
-            gpio_hold_en (port_mask (output[i]));
-   }
 }
 
 void
@@ -114,6 +106,7 @@ output_init (void)
             if (output[i] & PORT_INV)
                REVK_ERR_CHECK (gpio_set_level (p, 1));
             REVK_ERR_CHECK (gpio_set_direction (p, GPIO_MODE_OUTPUT));
+            gpio_hold_en (p);
          }
       }
 }
