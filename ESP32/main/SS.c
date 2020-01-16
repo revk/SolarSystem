3b08770b80acfd59e3c6c7b3eef7a94b2406ed84
Adrian Kennard <a@k.gg>
2020-01-16 06:53:00 +0000

    s/w restart not fault

--- ESP32/main/SS.c
+++ ESP32/main/SS.c
@@ -28,7 +28,7 @@ status_report (int force)
 #define m(n) extern const char *n##_fault;if(n##_fault){fault=n##_fault;module=#n;faults++;}
       modules
 #undef m
-         if (!fault && force && reason >= 0 && reason != ESP_RST_POWERON)
+         if (!fault && force && reason >= 0 && reason != ESP_RST_POWERON && reason != ESP_RST_SW)
       {
          module = "Boot";
          if (reason == ESP_RST_INT_WDT)
