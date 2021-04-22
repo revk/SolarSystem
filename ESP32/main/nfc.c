d79f5a8d979a7ce3ea192cedcb38a5e4b73fbe6c
Adrian Kennard <a@k.gg>
2021-04-22 22:35:38 +0100

    Tweak

--- ESP32/main/nfc.c
+++ ESP32/main/nfc.c
@@ -96,11 +96,11 @@ static void task(void *pvParameters)
          {                      // Failed
             // Try init again
             pn532_end(pn532);
-	    sleep(1);
+	    usleep(100000);
             pn532 = pn532_init(nfcuart, port_mask(nfctx), port_mask(nfcrx), nfcmask);
             if (!pn532)
-            {                   // Retry
-               sleep(1);
+            {                   // Retry before declaring a fault
+               usleep(1);
                pn532 = pn532_init(nfcuart, port_mask(nfctx), port_mask(nfcrx), nfcmask);
             }
             if (!pn532)
