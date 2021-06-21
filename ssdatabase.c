077ee3470fa3ed0459a741096a9c26f3dea3f760
Adrian Kennard <a@k.gg>
2021-06-21 11:31:43 +0100

    Start on database

--- ssdatabase.c
+++ ssdatabase.c
@@ -10,7 +10,7 @@
 #include <err.h>
 #include "SQLlib/sqllib.h"
 
-void ssdatabase(SQL * sqlp,const char *sqldatabase)
+void ssdatabase(SQL * sqlp, const char *sqldatabase)
 {                               // Check database integrity
    if (sql_select_db(sqlp, sqldatabase))
    {
@@ -20,32 +20,75 @@ void ssdatabase(SQL * sqlp,const char *sqldatabase)
    }
 
    SQL_RES *res = NULL;
+   const char *tablename = NULL;
    void endtable(void) {
       if (res)
          sql_free_result(res);
       res = NULL;
+      tablename = NULL;
    }
 
-   void table(const char *name) {
-      endtable();
-      res = sql_query_store(sqlp, sql_printf("SELECT * FROM `%#S` LIMIT 0", name));
+   void create(const char *name, int l) {
+      res = sql_query_store_free(sqlp, sql_printf("DESCRIBE `%S`", name));
       if (res)
-         return;                // Exists
+      {                         // Exists
+         sql_free_result(res);
+         res = NULL;
+         return;
+      }
       warnx("Creating table %s", name);
-      sql_safe_query_free(sqlp, sql_printf("CREATE TABLE `%#S` (`%#S` int unsigned auto_increment primary key)", name, name));
-      res = sql_query_store(sqlp, sql_printf("SELECT * FROM `%#S` LIMIT 0", name));
+      if (l)
+         sql_safe_query_free(sqlp, sql_printf("CREATE TABLE `%#S` (`%#S` CHAR(%d) NOT NULL PRIMARY KEY)", name, name, l));
+      else
+         sql_safe_query_free(sqlp, sql_printf("CREATE TABLE `%#S` (`%#S` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY)", name, name));
+   }
+
+   void table(const char *name, int l) {
+      endtable();
+      tablename = name;
+      l = l;
+      res = sql_safe_query_store_free(sqlp, sql_printf("SELECT * FROM `%#S` LIMIT 0", name));
    }
 
    void link(const char *name) {
+      if (sql_colnum(res, name) >= 0)
+         return;                // Exists - we are not updating type for now
+      int l = 0;
+#define table(n,len) if(!strcmp(name,#n))l=len;
+#include "ssdatabase.h"
+      warnx("Creating link %s/%s", tablename, name);
+      if (l)
+         sql_safe_query_free(sqlp, sql_printf("ALTER TABLE `%#S` ADD `%#S` CHAR(%d) DEFAULT NULL", tablename, name, l));
+      else
+         sql_safe_query_free(sqlp, sql_printf("ALTER TABLE `%#S` ADD `%#S` INT UNSIGNED DEFAULT NULL", tablename, name));
+      sql_safe_query_free(sqlp, sql_printf("ALTER TABLE `%#S` ADD CONSTRAINT `%#S_%#S` FOREIGN KEY (%#S) REFERENCES `%#S` (%#S) ON DELETE CASCADE ON UPDATE CASCADE", tablename, tablename, name, name, name, name));
    }
 
-   void text(const char *name) {
+   void text(const char *name, int l) {
+      if (sql_colnum(res, name) >= 0)
+         return;                // Exists - we are not updating type for now
+      warnx("Creating field %s/%s", tablename, name);
+      if (l)
+         sql_safe_query_free(sqlp, sql_printf("ALTER TABLE `%#S` ADD `%#S` CHAR(%d) DEFAULT NULL", tablename, name, l));
+      else
+         sql_safe_query_free(sqlp, sql_printf("ALTER TABLE `%#S` ADD `%#S` INT UNSIGNED DEFAULT NULL", tablename, name));
    }
 
-#define table(n)	table(#n);
-#define link(n)		link(#n);
-#define	text(n)		text(#n);
+   void num(const char *name) {
+      if (sql_colnum(res, name) >= 0)
+         return;                // Exists - we are not updating type for now
+      warnx("Creating field %s/%s", tablename, name);
+      sql_safe_query_free(sqlp, sql_printf("ALTER TABLE `%#S` ADD `%#S` INT DEFAULT NULL", tablename, name));
+   }
+
+
    sql_transaction(sqlp);
+#define table(n,l)	create(#n,l);   // Make tables first
+#include "ssdatabase.h"
+#define table(n,l)	table(#n,l);
+#define link(n)		link(#n);
+#define	text(n,l)	text(#n,l);
+#define	num(n)		num(#n);
 #include "ssdatabase.h"
    endtable();
    sql_safe_commit(sqlp);
--- ssdatabase.h
+++ ssdatabase.h
@@ -1,29 +1,59 @@
 // Database template
 
 #ifndef table
-#define table(n)		// Table (n is name, and field name for auto increment id of table as primary key)
+#define table(n,l)              // Table (n is name, and field name for auto increment id if l=0, or fixed string len l, as primary key)
 #endif
 
 #ifndef	link
-#define	link(n)			// Foreign key link to table n
+#define	link(n)                 // Foreign key link to table n
 #endif
 
 #ifndef text
-#define text(n)			// Text field
+#define text(n,l)               // Text field (l set for fixed size)
 #endif
 
-table(user);
+#ifndef num
+#define num(n)                  // Simple numeric field (integer)
+#endif
 
-table(customer);
+table(user, 0);
 
-table(site);
+table(usersite, 0);
+link(user);
+link(site);
 
-table(fob);
+table(usercustomer, 0);
+link(user);
+link(customer);
 
-table(device);
+table(customer, 0);
 
-table(template);
+table(site, 0);
+link(customer);
+
+table(fob, 14);
+link(user);
+
+table(device, 12);
+link(site);
+
+table(template, 0);
+
+table(templategpio, 0);
+link(template);
+num(pin);
+num(gpio);
+text(description, 0);
+
+table(aid, 6);
+text(key, 32);
+
+table(fobaid, 0);
+link(fob);
+link(aid);
+text(key, 32);
 
 #undef table
 #undef link
 #undef text
+#undef num
