47d9d765801fac056ff809db321a921372a7efd8
Adrian Kennard <a@k.gg>
2021-07-05 17:32:23 +0100

    Suh

--- ssafile.c
+++ ssafile.c
@@ -1,6 +1,6 @@
 // Make Afile
 
-#define _GNU_SOURCE		/* See feature_test_macros(7) */
+#define _GNU_SOURCE             /* See feature_test_macros(7) */
 #include "config.h"
 #include <stdio.h>
 #include <string.h>
@@ -20,207 +20,193 @@
 #include "DESFireAES/include/desfireaes.h"
 #include "ESP32/main/areas.h"
 
-unsigned int
-makeafile (SQL_RES * res, unsigned char *afile)
-{				// Make afile (max 256 characters) and return crc
-  if (afile)
-    {				// Default if error
+unsigned int makeafile(SQL_RES * res, unsigned char *afile)
+{                               // Make afile (max 256 characters) and return crc
+   if (afile)
+   {                            // Default if error
       afile[0] = 1;
-      afile[1] = 0xFB;		// Block
-    }
-  unsigned char a[256];
-  int p = 1;
-  void add (unsigned char v)
-  {
-    if (p < 256)
-      a[p] = v;
-    p++;
-  }
-  void addbcd (unsigned char v)
-  {
-    add (((v / 10) << 4) + (v % 10));
-  }
-  void addbcd2 (unsigned short v)
-  {
-    addbcd (v / 100);
-    addbcd (v % 100);
-  }
-  if (!res || !sql_col (res, "access") || sql_col (res, "blocked"))
-    add (0xFB);
-  else
-    {
-      if (res && *sql_colz (res, "commit") == 't')
-	add (0xF0);
-      if (res && *sql_colz (res, "count") == 't')
-	add (0xF1);
-      if (res && *sql_colz (res, "armlate") == 't')
-	add (0xFA);
-      if (res && sql_col (res, "block"))
-	add (0xFB);
-      if (res && *sql_colz (res, "clock") == 't')
-	add (0xFC);
-      if (res && *sql_colz (res, "override") == 't')
-	add (0xFF);
+      afile[1] = 0xFB;          // Block
+   }
+   unsigned char a[256];
+   int p = 1;
+   void add(unsigned char v) {
+      if (p < 256)
+         a[p] = v;
+      p++;
+   }
+   void addbcd(unsigned char v) {
+      add(((v / 10) << 4) + (v % 10));
+   }
+   void addbcd2(unsigned short v) {
+      addbcd(v / 100);
+      addbcd(v % 100);
+   }
+   if (!res || !sql_col(res, "access") || sql_col(res, "blocked"))
+      add(0xFB);
+   else
+   {
+      if (res && *sql_colz(res, "commit") == 't')
+         add(0xF0);
+      if (res && *sql_colz(res, "count") == 't')
+         add(0xF1);
+      if (res && *sql_colz(res, "armlate") == 't')
+         add(0xFA);
+      if (res && sql_col(res, "block"))
+         add(0xFB);
+      if (res && *sql_colz(res, "clock") == 't')
+         add(0xFC);
+      if (res && *sql_colz(res, "override") == 't')
+         add(0xFF);
       time_t expires = 0;
-      int expiry = atoi (sql_colz (res, "expiry"));
+      int expiry = atoi(sql_colz(res, "expiry"));
       if (expiry > 255)
-	expiry = 255;
+         expiry = 255;
       if (res)
-	expires = sql_time_utc (sql_colz (res, "expires"));
+         expires = sql_time_utc(sql_colz(res, "expires"));
       if (expires)
-	expiry = 0;		// Hard expiry instead
+         expiry = 0;            // Hard expiry instead
       if (expiry)
-	expires = time (0) + 86400 * expiry;
+         expires = time(0) + 86400 * expiry;
       if (expires)
-	{
-	  if (expiry)
-	    {
-	      add (0x31);
-	      add (expiry);
-	    }
-	  struct tm tm;
-	  gmtime_r (&expires, &tm);
-	  if (expiry || (!tm.tm_hour && !tm.tm_min && !tm.tm_sec) || (tm.tm_hour == 23 && tm.tm_min == 59 && tm.tm_sec == 59))
-	    {			// date only
-	      if (!expiry)
-		tm.tm_sec--;
-	      expires = timegm (&tm);
-	      gmtime_r (&expires, &tm);
-	      add (0x34);
-	      addbcd2 (tm.tm_year + 1900);
-	      addbcd (tm.tm_mon + 1);
-	      addbcd (tm.tm_mday);
-	    }
-	  else
-	    {
-	      add (0x37);
-	      addbcd2 (tm.tm_year + 1900);
-	      addbcd (tm.tm_mon + 1);
-	      addbcd (tm.tm_mday);
-	      addbcd (tm.tm_hour);
-	      addbcd (tm.tm_min);
-	      addbcd (tm.tm_sec);
-	    }
-	}
+      {
+         if (expiry)
+         {
+            add(0x31);
+            add(expiry);
+         }
+         struct tm tm;
+         gmtime_r(&expires, &tm);
+         if (expiry || (!tm.tm_hour && !tm.tm_min && !tm.tm_sec) || (tm.tm_hour == 23 && tm.tm_min == 59 && tm.tm_sec == 59))
+         {                      // date only
+            if (!expiry)
+               tm.tm_sec--;
+            expires = timegm(&tm);
+            gmtime_r(&expires, &tm);
+            add(0x34);
+            addbcd2(tm.tm_year + 1900);
+            addbcd(tm.tm_mon + 1);
+            addbcd(tm.tm_mday);
+         } else
+         {
+            add(0x37);
+            addbcd2(tm.tm_year + 1900);
+            addbcd(tm.tm_mon + 1);
+            addbcd(tm.tm_mday);
+            addbcd(tm.tm_hour);
+            addbcd(tm.tm_min);
+            addbcd(tm.tm_sec);
+         }
+      }
       static const char *days[] = { "sun", "mon", "tue", "wed", "thu", "fri", "sat" };
       unsigned short from[7] = { }, to[7] = { };
       int d;
       for (d = 0; d < 7; d++)
-	{
-	  char day[10];
-	  sprintf (day, "%sfrom", days[d]);
-	  const char *v = sql_colz (res, day);
-	  if (strlen (v) >= 5)
-	    from[d] = ((v[0] - '0') * 1000) + ((v[1] - '0') * 100) + ((v[3] - '0') * 10) + (v[4] - '0');
-	  sprintf (day, "%sto", days[d]);
-	  v = sql_colz (res, day);
-	  if (strlen (v) >= 5)
-	    {
-	      to[d] = ((v[0] - '0') * 1000) + ((v[1] - '0') * 100) + ((v[3] - '0') * 10) + (v[4] - '0');
-	      to[d]++;
-	      if ((to[d] % 100) == 60)
-		to[d] += 40;
-	    }
-	}
-      void addtimes (unsigned short day[7], unsigned char tag)
       {
-	for (d = 1; d < 7 && day[d] == day[0]; d++);
-	if (d == 7)
-	  {			// All same so one entry
-	    add (tag + 2);
-	    addbcd2 (day[0]);
-	  }
-	else
-	  {
-	    for (d = 2; d < 6 && day[d] == day[1]; d++);
-	    if (d == 6)
-	      {			// weekdays same
-		if (day[0] == day[6])
-		  {		// weekend same
-		    add (tag + 4);
-		    addbcd2 (day[0]);
-		    addbcd2 (day[1]);
-		  }
-		else
-		  {		// different weekend days
-		    add (tag + 6);
-		    addbcd2 (day[0]);
-		    addbcd2 (day[1]);
-		    addbcd2 (day[6]);
-		  }
-	      }
-	    else if (d == 5)
-	      {			// Sun,Mon-Thu,Fri,Say
-		add (tag + 8);
-		addbcd2 (day[0]);
-		addbcd2 (day[1]);
-		addbcd2 (day[5]);
-		addbcd2 (day[6]);
-	      }
-	    else
-	      {			// different days
-		add (tag + 14);
-		for (d = 0; d < 7; d++)
-		  addbcd2 (day[d]);
-	      }
-	  }
+         char day[10];
+         sprintf(day, "%sfrom", days[d]);
+         const char *v = sql_colz(res, day);
+         if (strlen(v) >= 5)
+            from[d] = ((v[0] - '0') * 1000) + ((v[1] - '0') * 100) + ((v[3] - '0') * 10) + (v[4] - '0');
+         sprintf(day, "%sto", days[d]);
+         v = sql_colz(res, day);
+         if (strlen(v) >= 5)
+         {
+            to[d] = ((v[0] - '0') * 1000) + ((v[1] - '0') * 100) + ((v[3] - '0') * 10) + (v[4] - '0');
+            to[d]++;
+            if ((to[d] % 100) == 60)
+               to[d] += 40;
+         }
+      }
+      void addtimes(unsigned short day[7], unsigned char tag) {
+         for (d = 1; d < 7 && day[d] == day[0]; d++);
+         if (d == 7)
+         {                      // All same so one entry
+            add(tag + 2);
+            addbcd2(day[0]);
+         } else
+         {
+            for (d = 2; d < 6 && day[d] == day[1]; d++);
+            if (d == 6)
+            {                   // weekdays same
+               if (day[0] == day[6])
+               {                // weekend same
+                  add(tag + 4);
+                  addbcd2(day[0]);
+                  addbcd2(day[1]);
+               } else
+               {                // different weekend days
+                  add(tag + 6);
+                  addbcd2(day[0]);
+                  addbcd2(day[1]);
+                  addbcd2(day[6]);
+               }
+            } else if (d == 5)
+            {                   // Sun,Mon-Thu,Fri,Say
+               add(tag + 8);
+               addbcd2(day[0]);
+               addbcd2(day[1]);
+               addbcd2(day[5]);
+               addbcd2(day[6]);
+            } else
+            {                   // different days
+               add(tag + 14);
+               for (d = 0; d < 7; d++)
+                  addbcd2(day[d]);
+            }
+         }
       }
       for (d = 0; d < 7 && !from[d] && to[d] == 0x2400; d++);
       if (d < 7)
-	{
-	  addtimes (from, 0x10);
-	  addtimes (to, 0x20);
-	}
-    }
-  void addarea (unsigned char tag, const char *name)
-  {
-    const char *v = sql_col (res, name);
-    if (v)
       {
-	unsigned int a = 0;
-	while (*v)
-	  {
-	    const char *p = strchr (AREAS, *v);
-	    if (p)
-	      a |= (0x80000000 >> (p - AREAS));
-	    v++;
-	  }
-	if (!p)
-	  add (tag);
-	else if (!(a & 0xFFFFFF))
-	  {
-	    add (tag + 1);
-	    add (a >> 24);
-	  }
-	else if (!(a & 0xFFFF))
-	  {
-	    add (tag + 2);
-	    add (a >> 24);
-	    add (a >> 16);
-	  }
-	else if (!(a & 0xFF))
-	  {
-	    add (tag + 3);
-	    add (a >> 24);
-	    add (a >> 16);
-	    add (a >> 8);
-	  }
-	else
-	  {
-	    add (tag + 4);
-	    add (a >> 24);
-	    add (a >> 16);
-	    add (a >> 8);
-	    add (a);
-	  }
+         addtimes(from, 0x10);
+         addtimes(to, 0x20);
       }
-  }
-  addarea (0xA0, "arm");
-  addarea (0xD0, "disarm");
-  addarea (0xE0, "enter");
-  if (p >= 256)
-    return 0;			// Too big
-  *a = p - 1;
-  memcpy (afile, a, p);
-  return df_crc (*a, a + 1);
+   }
+   void addarea(unsigned char tag, const char *name) {
+      const char *v = sql_col(res, name);
+      if (v)
+      {
+         unsigned int a = 0;
+         while (*v)
+         {
+            const char *p = strchr(AREAS, *v);
+            if (p)
+               a |= (0x80000000 >> (p - AREAS));
+            v++;
+         }
+         if (!p)
+            add(tag);
+         else if (!(a & 0xFFFFFF))
+         {
+            add(tag + 1);
+            add(a >> 24);
+         } else if (!(a & 0xFFFF))
+         {
+            add(tag + 2);
+            add(a >> 24);
+            add(a >> 16);
+         } else if (!(a & 0xFF))
+         {
+            add(tag + 3);
+            add(a >> 24);
+            add(a >> 16);
+            add(a >> 8);
+         } else
+         {
+            add(tag + 4);
+            add(a >> 24);
+            add(a >> 16);
+            add(a >> 8);
+            add(a);
+         }
+      }
+   }
+   addarea(0xA0, "arm");
+   addarea(0xD0, "disarm");
+   addarea(0xE0, "enter");
+   if (p >= 256)
+      return 0;                 // Too big
+   *a = p - 1;
+   memcpy(afile, a, p);
+   return df_crc(*a, a + 1);
 }
--- sscert.c
+++ sscert.c
@@ -16,182 +16,176 @@
 #include <openssl/pem.h>
 #include "AJL/ajl.h"
 
-#define KEYLEN  2048		// Default key length (we want to fit in an MQTT message)
+#define KEYLEN  2048            // Default key length (we want to fit in an MQTT message)
 
-EVP_PKEY *
-der2pkey (const char *der)
+EVP_PKEY *der2pkey(const char *der)
 {
-  uint8_t *buf;
-  ssize_t len = j_base64d (der, &buf);
-  if (len < 0)
-    errx (1, "Bad DER %s", der);
-  BIO *mem = BIO_new_mem_buf (buf, len);
-  EVP_PKEY *key = d2i_PrivateKey_bio (mem, NULL);
-  if (!key)
-    errx (1, "Failed to make key from DER: %s", der);
-  BIO_free (mem);
-  free (buf);
-  return key;
+   uint8_t *buf;
+   ssize_t len = j_base64d(der, &buf);
+   if (len < 0)
+      errx(1, "Bad DER %s", der);
+   BIO *mem = BIO_new_mem_buf(buf, len);
+   EVP_PKEY *key = d2i_PrivateKey_bio(mem, NULL);
+   if (!key)
+      errx(1, "Failed to make key from DER: %s", der);
+   BIO_free(mem);
+   free(buf);
+   return key;
 }
 
-X509 *
-der2x509 (const char *der)
+X509 *der2x509(const char *der)
 {
-  uint8_t *buf;
-  ssize_t len = j_base64d (der, &buf);
-  if (len < 0)
-    errx (1, "Bad DER %s", der);
-  BIO *mem = BIO_new_mem_buf (buf, len);
-  X509 *x509 = d2i_X509_bio (mem, NULL);
-  if (!x509)
-    errx (1, "Failed to make cert from DER: %s", der);
-  BIO_free (mem);
-  free (buf);
-  return x509;
+   uint8_t *buf;
+   ssize_t len = j_base64d(der, &buf);
+   if (len < 0)
+      errx(1, "Bad DER %s", der);
+   BIO *mem = BIO_new_mem_buf(buf, len);
+   X509 *x509 = d2i_X509_bio(mem, NULL);
+   if (!x509)
+      errx(1, "Failed to make cert from DER: %s", der);
+   BIO_free(mem);
+   free(buf);
+   return x509;
 }
 
-char *
-certpem (const char *der)
-{				// Make a PEM
-  X509 *cert = der2x509 (der);
-  char *buf;
-  size_t len;
-  FILE *fp = open_memstream (&buf, &len);
-  PEM_write_X509 (fp, cert);
-  fclose (fp);
-  X509_free (cert);
-  return buf;
+char *certpem(const char *der)
+{                               // Make a PEM
+   X509 *cert = der2x509(der);
+   char *buf;
+   size_t len;
+   FILE *fp = open_memstream(&buf, &len);
+   PEM_write_X509(fp, cert);
+   fclose(fp);
+   X509_free(cert);
+   return buf;
 }
 
-char *
-keypem (const char *der)
-{				// Make a PEM
-  EVP_PKEY *key = der2pkey (der);
-  char *buf;
-  size_t len;
-  FILE *fp = open_memstream (&buf, &len);
-  PEM_write_PrivateKey (fp, key, NULL, NULL, 0, NULL, NULL);
-  fclose (fp);
-  EVP_PKEY_free (key);
-  return buf;
+char *keypem(const char *der)
+{                               // Make a PEM
+   EVP_PKEY *key = der2pkey(der);
+   char *buf;
+   size_t len;
+   FILE *fp = open_memstream(&buf, &len);
+   PEM_write_PrivateKey(fp, key, NULL, NULL, 0, NULL, NULL);
+   fclose(fp);
+   EVP_PKEY_free(key);
+   return buf;
 }
 
-char *
-makekey (void)
-{				// Make a key and return (malloc'd) DER private key
-  EVP_PKEY *pkey;
-  pkey = EVP_PKEY_new ();
-  BIGNUM *bn;
-  bn = BN_new ();
-  BN_set_word (bn, RSA_F4);
-  RSA *rsa;
-  rsa = RSA_new ();
-  RAND_poll ();
-  RSA_generate_key_ex (rsa, KEYLEN, bn, NULL);
-  EVP_PKEY_assign_RSA (pkey, rsa);
-  uint8_t *buf = NULL;
-  int len = i2d_PrivateKey (pkey, &buf);
-  if (len < 0)
-    errx (1, "Bad private key DER");
-  char *key = strdup (j_base64 (len, buf));
-  free (buf);
-  EVP_PKEY_free (pkey);
-  return key;
+char *makekey(void)
+{                               // Make a key and return (malloc'd) DER private key
+   EVP_PKEY *pkey;
+   pkey = EVP_PKEY_new();
+   BIGNUM *bn;
+   bn = BN_new();
+   BN_set_word(bn, RSA_F4);
+   RSA *rsa;
+   rsa = RSA_new();
+   RAND_poll();
+   RSA_generate_key_ex(rsa, KEYLEN, bn, NULL);
+   EVP_PKEY_assign_RSA(pkey, rsa);
+   uint8_t *buf = NULL;
+   int len = i2d_PrivateKey(pkey, &buf);
+   if (len < 0)
+      errx(1, "Bad private key DER");
+   char *key = strdup(j_base64(len, buf));
+   free(buf);
+   EVP_PKEY_free(pkey);
+   return key;
 }
 
-char *
-makecert (const char *keyder, const char *cakeyder, const char *cacertder, const char *name)
-{				// Make a cert and return (malloc'd) DER - self signed if certder is NULL
-  EVP_PKEY *key = NULL, *cakey = NULL;
-  X509 *cacert = NULL;
-  if (keyder && *keyder)
-    key = der2pkey (keyder);
-  if (cakeyder && *cakeyder)
-    cakey = der2pkey (cakeyder);
-  if (cacertder && *cacertder)
-    cacert = der2x509 (cacertder);
+char *makecert(const char *keyder, const char *cakeyder, const char *cacertder, const char *name)
+{                               // Make a cert and return (malloc'd) DER - self signed if certder is NULL
+   EVP_PKEY *key = NULL,
+       *cakey = NULL;
+   X509 *cacert = NULL;
+   if (keyder && *keyder)
+      key = der2pkey(keyder);
+   if (cakeyder && *cakeyder)
+      cakey = der2pkey(cakeyder);
+   if (cacertder && *cacertder)
+      cacert = der2x509(cacertder);
 #define nz(x) do if(!(x))errx(1,"Failed %s",#x); while(0)
-  // Make cert
-  X509 *cert = X509_new ();
-  if (!cert)
-    errx (1, "Failed to make cert");
-  nz (X509_set_version (cert, 2));
-  ASN1_INTEGER *aserial = ASN1_INTEGER_new ();
-  ASN1_INTEGER_set (aserial, time (0));
-  nz (X509_set_serialNumber (cert, aserial));
-  ASN1_INTEGER_free (aserial);
-  X509_NAME *subject = X509_NAME_new ();
-  nz (X509_NAME_add_entry_by_txt (subject, "C", MBSTRING_ASC, (const unsigned char *) "UK", -1, -1, 0));
-  nz (X509_NAME_add_entry_by_txt (subject, "O", MBSTRING_ASC, (const unsigned char *) "Solar System", -1, -1, 0));
-  if (name && *name)
-    nz (X509_NAME_add_entry_by_txt (subject, "CN", MBSTRING_ASC, (const unsigned char *) name, -1, -1, 0));
-  nz (X509_set_subject_name (cert, subject));
-  if (cacert)
-    nz (X509_set_issuer_name (cert, X509_get_subject_name (cacert)));
-  else
-    nz (X509_set_issuer_name (cert, subject));	// Self signed
-  X509_NAME_free (subject);
-  nz (X509_set_pubkey (cert, key));
-  nz (X509_gmtime_adj (X509_get_notBefore (cert), 0));
-  nz (X509_gmtime_adj (X509_get_notAfter (cert), 50 * 365 * 86400));	// Long long time in the future
-  X509V3_CTX ctx;
-  X509V3_set_ctx_nodb (&ctx);
-  X509V3_set_ctx (&ctx, cacert, cert, NULL, NULL, 0);
-  if (!cacert)
-    nz (X509_add_ext (cert, X509V3_EXT_conf_nid (NULL, &ctx, NID_basic_constraints, "critical, CA:TRUE"), -1));
-  else
-    nz (X509_add_ext (cert, X509V3_EXT_conf_nid (NULL, &ctx, NID_issuer_alt_name, "issuer:copy"), -1));
-  nz (X509_add_ext (cert, X509V3_EXT_conf_nid (NULL, &ctx, NID_ext_key_usage, "critical, clientAuth, serverAuth"), -1));
-  nz (X509_add_ext (cert, X509V3_EXT_conf_nid (NULL, &ctx, NID_subject_key_identifier, "hash"), -1));
-  // Sign
-  nz (X509_sign (cert, cakey ? : key, EVP_sha256 ()));
-  // Write cert
-  uint8_t *buf = NULL;
-  int len = i2d_X509 (cert, &buf);
-  if (len < 0)
-    errx (1, "Bad cert DER");
-  char *der_cert = strdup (j_base64 (len, buf));
-  free (buf);
-  X509_free (cert);
-  if (cacert)
-    X509_free (cacert);
-  if (key)
-    EVP_PKEY_free (key);
-  if (cakey)
-    EVP_PKEY_free (cakey);
+   // Make cert
+   X509 *cert = X509_new();
+   if (!cert)
+      errx(1, "Failed to make cert");
+   nz(X509_set_version(cert, 2));
+   ASN1_INTEGER *aserial = ASN1_INTEGER_new();
+   ASN1_INTEGER_set(aserial, time(0));
+   nz(X509_set_serialNumber(cert, aserial));
+   ASN1_INTEGER_free(aserial);
+   X509_NAME *subject = X509_NAME_new();
+   nz(X509_NAME_add_entry_by_txt(subject, "C", MBSTRING_ASC, (const unsigned char *) "UK", -1, -1, 0));
+   nz(X509_NAME_add_entry_by_txt(subject, "O", MBSTRING_ASC, (const unsigned char *) "Solar System", -1, -1, 0));
+   if (name && *name)
+      nz(X509_NAME_add_entry_by_txt(subject, "CN", MBSTRING_ASC, (const unsigned char *) name, -1, -1, 0));
+   nz(X509_set_subject_name(cert, subject));
+   if (cacert)
+      nz(X509_set_issuer_name(cert, X509_get_subject_name(cacert)));
+   else
+      nz(X509_set_issuer_name(cert, subject));  // Self signed
+   X509_NAME_free(subject);
+   nz(X509_set_pubkey(cert, key));
+   nz(X509_gmtime_adj(X509_get_notBefore(cert), 0));
+   nz(X509_gmtime_adj(X509_get_notAfter(cert), 50 * 365 * 86400));      // Long long time in the future
+   X509V3_CTX ctx;
+   X509V3_set_ctx_nodb(&ctx);
+   X509V3_set_ctx(&ctx, cacert, cert, NULL, NULL, 0);
+   if (!cacert)
+      nz(X509_add_ext(cert, X509V3_EXT_conf_nid(NULL, &ctx, NID_basic_constraints, "critical, CA:TRUE"), -1));
+   else
+      nz(X509_add_ext(cert, X509V3_EXT_conf_nid(NULL, &ctx, NID_issuer_alt_name, "issuer:copy"), -1));
+   nz(X509_add_ext(cert, X509V3_EXT_conf_nid(NULL, &ctx, NID_ext_key_usage, "critical, clientAuth, serverAuth"), -1));
+   nz(X509_add_ext(cert, X509V3_EXT_conf_nid(NULL, &ctx, NID_subject_key_identifier, "hash"), -1));
+   // Sign
+   nz(X509_sign(cert, cakey ? : key, EVP_sha256()));
+   // Write cert
+   uint8_t *buf = NULL;
+   int len = i2d_X509(cert, &buf);
+   if (len < 0)
+      errx(1, "Bad cert DER");
+   char *der_cert = strdup(j_base64(len, buf));
+   free(buf);
+   X509_free(cert);
+   if (cacert)
+      X509_free(cacert);
+   if (key)
+      EVP_PKEY_free(key);
+   if (cakey)
+      EVP_PKEY_free(cakey);
 #undef nz
-  return der_cert;
+   return der_cert;
 }
 
 #ifndef	LIB
-int
-main (int argc, const char *argv[])
+int main(int argc, const char *argv[])
 {
-  FILE *f;
+   FILE *f;
 
-  argc = argc;
-  argv = argv;
-  warnx ("Testing certificate stuff");
-  char *cakey = makekey ();
-  char *cacert = makecert (cakey, NULL, NULL, "SolarSystem");
-  char *sitekey = makekey ();
-  char *sitecert = makecert (sitekey, cakey, cacert, "localhost");
+   argc = argc;
+   argv = argv;
+   warnx("Testing certificate stuff");
+   char *cakey = makekey();
+   char *cacert = makecert(cakey, NULL, NULL, "SolarSystem");
+   char *sitekey = makekey();
+   char *sitecert = makecert(sitekey, cakey, cacert, "localhost");
 
-  f = fopen ("/tmp/cakey.pem", "w");
-  fprintf (f, "%s", keypem (cakey));
-  fclose (f);
-  f = fopen ("/tmp/cacert.pem", "w");
-  fprintf (f, "%s", certpem (cacert));
-  fclose (f);
-  f = fopen ("/tmp/sitekey.pem", "w");
-  fprintf (f, "%s", keypem (sitekey));
-  fclose (f);
-  f = fopen ("/tmp/sitecert.pem", "w");
-  fprintf (f, "%s", certpem (sitecert));
-  fclose (f);
+   f = fopen("/tmp/cakey.pem", "w");
+   fprintf(f, "%s", keypem(cakey));
+   fclose(f);
+   f = fopen("/tmp/cacert.pem", "w");
+   fprintf(f, "%s", certpem(cacert));
+   fclose(f);
+   f = fopen("/tmp/sitekey.pem", "w");
+   fprintf(f, "%s", keypem(sitekey));
+   fclose(f);
+   f = fopen("/tmp/sitecert.pem", "w");
+   fprintf(f, "%s", certpem(sitecert));
+   fclose(f);
 
-  //system("openssl s_server -key /tmp/cakey.pem -cert /tmp/cacert.pem -accept localhost:4443");
-  system ("openssl s_server -key /tmp/sitekey.pem -cert /tmp/sitecert.pem -accept localhost:4443");
+   //system("openssl s_server -key /tmp/cakey.pem -cert /tmp/cacert.pem -accept localhost:4443");
+   system("openssl s_server -key /tmp/sitekey.pem -cert /tmp/sitecert.pem -accept localhost:4443");
 
 }
 #endif
