d298fe92b4555fb34ae85d8aed352c735121677b
Adrian Kennard <a@k.gg>
2021-07-09 17:06:16 +0100

    Tidy

--- ESP32/main/SS.c
+++ ESP32/main/SS.c
@@ -173,21 +173,20 @@ const char *port_check(int p, const char *module, int in)
 const char *app_callback(int client, const char *prefix, const char *target, const char *suffix, jo_t j)
 {
    const char *e = NULL;
-   if (!client && prefix && !strcmp(prefix, prefixcommand))
-   {                            // Commands from main MQTT
-      if (target && (!strcmp(target, revk_id) || !strcmp(target, "*")))
-      {                         // To us
+   if (client || !prefix || target || strcmp(prefix, prefixcommand))
+      return NULL;                    // Not for us or not a command from main MQTT
+   if (target && (!strcmp(target, revk_id) || !strcmp(target, "*")))
+   {                            // To us
 #define m(x) extern const char * x##_command(const char *,jo_t); jo_rewind(j);if(!e)e=x##_command(suffix,j);
-         modules;
+      modules;
 #undef m
-      }
-      if (!target)
-      {                         // System commands
-         if (!strcmp(suffix, "connect"))
-         {
-            status_report(1);
-            status_report(0);
-         }
+   }
+   if (!target)
+   {                            // System commands
+      if (!strcmp(suffix, "connect"))
+      {
+         status_report(1);
+         status_report(0);
       }
    }
    return e;
