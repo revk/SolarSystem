f6cda29728db6b22f98289353366000aadae5a04
RevK <info@revk.uk>
2021-09-03 09:09:07 +0100

    Out of service is any channel

--- solarsystem.c
+++ solarsystem.c
@@ -404,7 +404,7 @@ addsitedata (SQL * sqlp, j_t j, SQL_RES * site, const char *deviceid, const char
     j_store_string (wifi, "ssid", v);
   if ((v = sql_colz (site, "wifipass")) && *v)
     j_store_string (wifi, "pass", v);
-  if ((v = sql_colz (site, "wifichan")) && atoi (v))
+  if (!outofservice && (v = sql_colz (site, "wifichan")) && atoi (v))
     j_store_int (wifi, "chan", atoi (v));
   j_t sms = j_store_object (j, "sms");
   addarea (sms, "arm", sql_col (site, "smsarm"), 0);
