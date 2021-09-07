86e1dc5e5b9b6a56de392c2036689b6b32b77b20
Adrian Kennard <a@k.gg>
2021-09-07 09:12:43 +0100

    Events for warning,panic,tamper,fault

--- ESP32/main/alarm.c
+++ ESP32/main/alarm.c
@@ -364,6 +364,15 @@ const char *mesh_make_report(jo_t j)
             }
             presence |= inputtamper[i];
             presence |= inputaccess[i];
+            if ((latch | input_stable) & (1ULL << i))
+            { // Event reporting regardless
+#define i(x) if(input##x[i]){jo_t e = jo_make(NULL);jo_string(e, "input", inputname[i]);jo_area(e,#x,input##x[i]);revk_event_clients(#x, &e, 1);}
+               i(warning);
+               i(tamper);
+               i(fault);
+	       i(panic);
+#undef i
+            }
          }
       }
    }
