d5ec3516b7f1c81c2bfd064c79c130d6b57da347
Adrian Kennard <a@k.gg>
2021-09-19 06:39:57 +0100

    Debug

--- ESP32/main/alarm.c
+++ ESP32/main/alarm.c
@@ -651,7 +651,6 @@ static void mesh_handle_report(const char *target, jo_t j)
    char dev[17] = "";
    jo_type_t t;
    jo_rewind(j);
-   ESP_LOGI(TAG, "Report(%d): %s", node[child].part, jo_debug(j));
    for (jo_next(j); (t = jo_here(j)) > JO_CLOSE; jo_skip(j))
       if (t == JO_TAG)
       {
