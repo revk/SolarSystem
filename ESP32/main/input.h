fb8120c94446377a7f36d02eb6a3dbe0f3ff0f43
Adrian Kennard <a@k.gg>
2021-09-24 09:21:42 +0100

    Comments

--- ESP32/main/input.h
+++ ESP32/main/input.h
@@ -5,8 +5,8 @@
 typedef uint16_t input_t;       // Enough for number of inputs
 
 extern input_t input_stable;    // holds current input state
-extern input_t input_latch;     // holds resettable state of input
-extern input_t input_flip;      // holds flipped flag for each input, i.e. state has changed
+extern input_t input_latch;     // inputs have changed state to on at some point
+extern input_t input_flip;      // inputs have changed state at some point
 extern char *inputname[MAXINPUT];
 #define i(t,x,c) extern area_t input##x[MAXINPUT];
 #include "states.m"
