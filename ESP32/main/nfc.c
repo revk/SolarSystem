59fed730c2cbd8b72166edd1ecff29511ca55b75
RevK <github@k.gg>
2023-04-05 10:34:32 +0100

    Inverted NFC rx option working now

--- ESP32/main/nfc.c
+++ ESP32/main/nfc.c
@@ -195,6 +195,8 @@ static void task(void *pvParameters)
    uint8_t ledpos = 0;
    uint8_t retry = 0;
    uint8_t holdpolls = 0;
+   uart_set_line_inverse(nfcuart, ((PORT_INV & nfctx) ? UART_SIGNAL_TXD_INV : 0) + ((PORT_INV & nfcrx) ? UART_SIGNAL_RXD_INV : 0));     // Allow for inverted pins
+   gpio_set_pull_mode(port_mask(nfcrx), (PORT_INV & nfcrx) ? GPIO_PULLUP_ONLY : GPIO_FLOATING); // If inverted rx, set pull up, as will be a FET
    while (1)
    {
       esp_task_wdt_reset();
@@ -239,8 +241,6 @@ static void task(void *pvParameters)
                   pn532 = pn532_init(nfcuart, port_mask(nfctx), port_mask(nfcrx), nfcmask);
                if (pn532)
                {                // All good!
-                  uart_set_line_inverse(nfcuart, ((PORT_INV & nfctx) ? UART_SIGNAL_TXD_INV : 0) + ((PORT_INV & nfcrx) ? UART_SIGNAL_RXD_INV : 0));      // Allow for inverted pins
-                  gpio_set_pull_mode(port_mask(nfcrx), (PORT_INV & nfcrx) ? GPIO_PULLUP_ONLY : GPIO_FLOATING);  // If inverted rx, set pull up, as will be a FET
                   df_init(&df, pn532, pn532_dx);
                   ledlast = 0xFF;
                   logical_gpio &= ~logical_NFCFault;
