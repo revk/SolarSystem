2d1b3114d0636062f12432c646604864c2a175f7
RevK <github@k.gg>
2023-07-18 09:14:08 +0100

    FFS

--- ESP32/main/alarm.c
+++ ESP32/main/alarm.c
@@ -1013,7 +1013,7 @@ task (void *pvParameters)
    nodes_online++;
    while (1)
    {
-      if (mqttdied && isroot && uptime () > mqttdied && lwmqtt_failed (lwmqtt_client (0)) > 5 && !revk_shutting_down (NULL))
+      if (mqttdied && isroot && uptime () > mqttdied && lwmqtt_failed (revk_mqtt (0)) > 5 && !revk_shutting_down (NULL))
          revk_restart ("MQTT not connecting", 0);
       esp_task_wdt_reset ();
       {                         // Timer logic input
