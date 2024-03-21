c7da125427c214184bc471034d225e686914c596
RevK <github@k.gg>
2024-03-21 11:27:19 +0000

    FFS

--- ESP32/main/input.c
+++ ESP32/main/input.c
@@ -88,7 +88,7 @@ task (void *pvParameters)
 #ifdef  CONFIG_REVK_LED_STRIP
    for (int i = 0; i < MAXINPUT; i++)
       if (inrgb[i])
-         led_set (inrgb[i], ((1ULL << i) & input_raw) ? 'G' : 'R');
+         led_set (inrgb[i], ((1ULL << i) & input_raw) ? 'R' : 'G');
 #endif
    // Scan inputs
    while (1)
