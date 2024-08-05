e251fd7076e1d49d52ca99663e2391ea65db7d1d
RevK <github@k.gg>
2024-08-05 09:14:44 +0100

    Comments

--- ESP32/main/SS.c
+++ ESP32/main/SS.c
@@ -19,19 +19,19 @@ static const char __attribute__((unused)) TAG[] = "SS";
 #warning	You do not want door controller running CONFIG_REVK_APMODE
 #endif
 
-#ifdef	CONFIG_ESP32_WIFI_AMPDU_TX_ENABLED
-#warning CONFIG_ESP32_WIFI_AMPDU_TX_ENABLED may not be a good idea
-#endif
-#ifdef	CONFIG_ESP32_WIFI_AMPDU_RX_ENABLED
-#warning CONFIG_ESP32_WIFI_AMPDU_RX_ENABLED may not be a good idea
-#endif
+//#ifdef	CONFIG_ESP32_WIFI_AMPDU_TX_ENABLED
+//#warning CONFIG_ESP32_WIFI_AMPDU_TX_ENABLED may not be a good idea
+//#endif
+//#ifdef	CONFIG_ESP32_WIFI_AMPDU_RX_ENABLED
+//#warning CONFIG_ESP32_WIFI_AMPDU_RX_ENABLED may not be a good idea
+//#endif
 
 #ifndef CONFIG_ESP32_WIFI_STATIC_TX_BUFFER
 #warning CONFIG_ESP32_WIFI_STATIC_TX_BUFFER may be better
 #else
-#if CONFIG_ESP32_WIFI_STATIC_RX_BUFFER_NUM+CONFIG_ESP32_WIFI_DYNAMIC_RX_BUFFER_NUM+CONFIG_ESP32_WIFI_STATIC_TX_BUFFER_NUM > 30
-#warning Maybe fewer wifi buffers
-#endif
+//#if CONFIG_ESP32_WIFI_STATIC_RX_BUFFER_NUM+CONFIG_ESP32_WIFI_DYNAMIC_RX_BUFFER_NUM+CONFIG_ESP32_WIFI_STATIC_TX_BUFFER_NUM > 30
+//#warning Maybe fewer wifi buffers
+//#endif
 #endif
 
 #ifndef	CONFIG_REVK_MESH
