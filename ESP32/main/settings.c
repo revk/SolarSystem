5b73220fede6d0912732c823c0dbb21bcb653b30
RevK <github@k.gg>
2025-07-30 09:09:42 +0100

    Latest library

--- ESP32/main/settings.c
+++ ESP32/main/settings.c
@@ -18,37 +18,50 @@ revk_settings_bits_t revk_settings_bits={0};
 #define	quote(s)	str(s)
 revk_settings_t const revk_settings[]={
 #ifdef	CONFIG_REVK_SETTINGS_PASSWORD
- {.type=REVK_SETTINGS_STRING,.name="password",.comment="Settings password (this is not sent securely so use with care on local networks you control)",.len=8,.ptr=&password,.malloc=1,.revk=1,.hide=1,.secret=1},
+ {.type=REVK_SETTINGS_STRING,.name="password",.comment="Settings password<br>(not sent securely so use with care)",.len=8,.ptr=&password,.malloc=1,.revk=1,.hide=1,.secret=1},
 #endif
- {.type=REVK_SETTINGS_STRING,.name="hostname",.comment="Host name",.len=8,.ptr=&hostname,.malloc=1,.revk=1,.hide=1},
+#ifdef  CONFIG_MDNS_MAX_INTERFACES
+ {.type=REVK_SETTINGS_STRING,.name="hostname",.comment="Hostname[.local]<br>(used in DHCP and MQTT)",.len=8,.ptr=&hostname,.malloc=1,.revk=1,.hide=1},
+#else
+ {.type=REVK_SETTINGS_STRING,.name="hostname",.comment="Host name<br>(used in DHCP and MQTT)",.len=8,.ptr=&hostname,.malloc=1,.revk=1,.hide=1},
+#endif
  {.type=REVK_SETTINGS_STRING,.name="appname",.comment="Application name",.len=7,.dq=1,.def=quote(CONFIG_REVK_APPNAME),.ptr=&appname,.malloc=1,.revk=1,.hide=1},
  {.type=REVK_SETTINGS_STRING,.name="otahost",.comment="OTA hostname",.group=1,.len=7,.dot=3,.dq=1,.def=quote(CONFIG_REVK_OTAHOST),.ptr=&otahost,.malloc=1,.revk=1,.live=1},
  {.type=REVK_SETTINGS_UNSIGNED,.name="otadays",.comment="OTA auto load (days)",.group=1,.len=7,.dot=3,.dq=1,.def=quote(CONFIG_REVK_OTADAYS),.ptr=&otadays,.size=sizeof(uint8_t),.revk=1},
  {.type=REVK_SETTINGS_UNSIGNED,.name="otastart",.comment="OTA check after startup (min seconds)",.group=1,.len=8,.dot=3,.def="600",.ptr=&otastart,.size=sizeof(uint16_t),.revk=1},
  {.type=REVK_SETTINGS_BIT,.name="otaauto",.comment="OTA auto upgrade",.group=1,.len=7,.dot=3,.def="1",.bit=REVK_SETTINGS_BITFIELD_otaauto,.revk=1,.hide=1,.live=1},
+#ifdef	CONFIG_REVK_WEB_BETA
  {.type=REVK_SETTINGS_BIT,.name="otabeta",.comment="OTA from beta release",.group=1,.len=7,.dot=3,.bit=REVK_SETTINGS_BITFIELD_otabeta,.revk=1,.hide=1,.live=1},
+#endif
  {.type=REVK_SETTINGS_BLOB,.name="otacert",.comment="OTA cert of otahost",.group=1,.len=7,.dot=3,.dq=1,.def=quote(CONFIG_REVK_OTACERT),.ptr=&otacert,.malloc=1,.revk=1,.base64=1},
+ {.type=REVK_SETTINGS_BIT,.name="dark",.comment="Default LED off",.len=4,.bit=REVK_SETTINGS_BITFIELD_dark,.revk=1,.live=1},
  {.type=REVK_SETTINGS_STRING,.name="ntphost",.comment="NTP host",.len=7,.dq=1,.def=quote(CONFIG_REVK_NTPHOST),.ptr=&ntphost,.malloc=1,.revk=1},
  {.type=REVK_SETTINGS_STRING,.name="tz",.comment="Timezone (<a href='https://gist.github.com/alwynallan/24d96091655391107939' target=_blank>info</a>)",.len=2,.dq=1,.def=quote(CONFIG_REVK_TZ),.ptr=&tz,.malloc=1,.revk=1,.hide=1},
  {.type=REVK_SETTINGS_UNSIGNED,.name="watchdogtime",.comment="Watchdog (seconds)",.len=12,.dq=1,.def=quote(CONFIG_REVK_WATCHDOG),.ptr=&watchdogtime,.size=sizeof(uint32_t),.revk=1},
- {.type=REVK_SETTINGS_STRING,.name="topicgroup",.comment="MQTT Alternative hostname accepted for commands",.group=2,.len=10,.dot=5,.ptr=&topicgroup,.malloc=1,.revk=1,.array=2},
- {.type=REVK_SETTINGS_STRING,.name="topiccommand",.comment="MQTT Topic for commands",.group=2,.len=12,.dot=5,.def="command",.ptr=&topiccommand,.malloc=1,.revk=1,.old="prefixcommand"			},
- {.type=REVK_SETTINGS_STRING,.name="topicsetting",.comment="MQTT Topic for settings",.group=2,.len=12,.dot=5,.def="setting",.ptr=&topicsetting,.malloc=1,.revk=1,.old="prefixsetting"			},
- {.type=REVK_SETTINGS_STRING,.name="topicstate",.comment="MQTT Topic for state",.group=2,.len=10,.dot=5,.def="state",.ptr=&topicstate,.malloc=1,.revk=1,.old="prefixstate"			},
- {.type=REVK_SETTINGS_STRING,.name="topicevent",.comment="MQTT Topic for event",.group=2,.len=10,.dot=5,.def="event",.ptr=&topicevent,.malloc=1,.revk=1,.old="prefixevent"			},
- {.type=REVK_SETTINGS_STRING,.name="topicinfo",.comment="MQTT Topic for info",.group=2,.len=9,.dot=5,.def="info",.ptr=&topicinfo,.malloc=1,.revk=1,.old="prefixinfo"			},
- {.type=REVK_SETTINGS_STRING,.name="topicerror",.comment="MQTT Topic for error",.group=2,.len=10,.dot=5,.def="error",.ptr=&topicerror,.malloc=1,.revk=1,.old="prefixerror"			},
- {.type=REVK_SETTINGS_STRING,.name="topicha",.comment="MQTT Topic for homeassistant",.group=2,.len=7,.dot=5,.def="homeassistant",.ptr=&topicha,.malloc=1,.revk=1},
- {.type=REVK_SETTINGS_BIT,.name="prefixapp",.comment="MQTT use appname/ in front of hostname in topic",.group=3,.len=9,.dot=6,.dq=1,.def=quote(CONFIG_REVK_PREFIXAPP),.bit=REVK_SETTINGS_BITFIELD_prefixapp,.revk=1},
- {.type=REVK_SETTINGS_BIT,.name="prefixhost",.comment="MQTT use (appname/)hostname/topic instead of topic/(appname/)hostname",.group=3,.len=10,.dot=6,.dq=1,.def=quote(CONFIG_REVK_PREFIXHOST),.bit=REVK_SETTINGS_BITFIELD_prefixhost,.revk=1},
-#ifdef	CONFIG_REVK_BLINK_DEF
- {.type=REVK_SETTINGS_UNSIGNED,.gpio=1,.name="blink",.comment="R, G, B LED array (set all the same for WS2812 LED)",.len=5,.dq=1,.def=quote(CONFIG_REVK_BLINK),.ptr=&blink,.size=sizeof(revk_gpio_t),.fix=1,.set=1,.flags="- ~↓↕⇕",.revk=1,.array=3},
+#ifdef  CONFIG_IDF_TARGET_ESP32S3
+ {.type=REVK_SETTINGS_UNSIGNED,.name="usbuptime",.comment="USB  turns off after this many seconds",.len=9,.dq=1,.def=quote(CONFIG_REVK_USBUPTIME),.ptr=&usbuptime,.size=sizeof(uint16_t),.revk=1},
 #endif
- {.type=REVK_SETTINGS_BIT,.name="dark",.comment="Default LED off",.len=4,.bit=REVK_SETTINGS_BITFIELD_dark,.revk=1,.live=1},
 #ifdef  CONFIG_IDF_TARGET_ESP32S3
  {.type=REVK_SETTINGS_UNSIGNED,.gpio=1,.name="factorygpio",.comment="Factory reset GPIO (press 3 times)",.len=11,.def="-21",.ptr=&factorygpio,.size=sizeof(revk_gpio_t),.fix=1,.set=1,.flags="- ~↓↕⇕",.revk=1},
 #else
  {.type=REVK_SETTINGS_UNSIGNED,.gpio=1,.name="factorygpio",.comment="Factory reset GPIO (press 3 times)",.len=11,.ptr=&factorygpio,.size=sizeof(revk_gpio_t),.fix=1,.set=1,.flags="- ~↓↕⇕",.revk=1},
+#endif
+ {.type=REVK_SETTINGS_STRING,.name="topicgroup",.comment="MQTT Alternative hostname accepted for commands",.group=2,.len=10,.dot=5,.ptr=&topicgroup,.malloc=1,.revk=1,.array=2},
+ {.type=REVK_SETTINGS_STRING,.name="topiccommand",.comment="MQTT Topic for commands",.group=2,.len=12,.dot=5,.def="command",.ptr=&topiccommand,.malloc=1,.revk=1,.old="prefixcommand"},
+ {.type=REVK_SETTINGS_STRING,.name="topicsetting",.comment="MQTT Topic for settings",.group=2,.len=12,.dot=5,.def="setting",.ptr=&topicsetting,.malloc=1,.revk=1,.old="prefixsetting"},
+ {.type=REVK_SETTINGS_STRING,.name="topicstate",.comment="MQTT Topic for state",.group=2,.len=10,.dot=5,.def="state",.ptr=&topicstate,.malloc=1,.revk=1,.old="prefixstate"},
+ {.type=REVK_SETTINGS_STRING,.name="topicevent",.comment="MQTT Topic for event",.group=2,.len=10,.dot=5,.def="event",.ptr=&topicevent,.malloc=1,.revk=1,.old="prefixevent"},
+ {.type=REVK_SETTINGS_STRING,.name="topicinfo",.comment="MQTT Topic for info",.group=2,.len=9,.dot=5,.def="info",.ptr=&topicinfo,.malloc=1,.revk=1,.old="prefixinfo"},
+ {.type=REVK_SETTINGS_STRING,.name="topicerror",.comment="MQTT Topic for error",.group=2,.len=10,.dot=5,.def="error",.ptr=&topicerror,.malloc=1,.revk=1,.old="prefixerror"},
+ {.type=REVK_SETTINGS_STRING,.name="topicha",.comment="MQTT Topic for homeassistant",.group=2,.len=7,.dot=5,.def="homeassistant",.ptr=&topicha,.malloc=1,.revk=1},
+ {.type=REVK_SETTINGS_BIT,.name="prefixapp",.comment="MQTT use appname/ in front of hostname in topic",.group=3,.len=9,.dot=6,.dq=1,.def=quote(CONFIG_REVK_PREFIXAPP),.bit=REVK_SETTINGS_BITFIELD_prefixapp,.revk=1},
+ {.type=REVK_SETTINGS_BIT,.name="prefixhost",.comment="MQTT use (appname/)hostname/topic instead of topic/(appname/)hostname",.group=3,.len=10,.dot=6,.dq=1,.def=quote(CONFIG_REVK_PREFIXHOST),.bit=REVK_SETTINGS_BITFIELD_prefixhost,.revk=1},
+#ifdef	CONFIG_REVK_BLINK_DEF
+#ifdef	CONFIG_REVK_BLINK_WS2812_DEF
+ {.type=REVK_SETTINGS_UNSIGNED,.gpio=1,.name="blink",.comment="WS2812 LED",.len=5,.dq=1,.def=quote(CONFIG_REVK_BLINK),.ptr=&blink,.size=sizeof(revk_gpio_t),.fix=1,.set=1,.flags="- ~↓↕⇕",.revk=1},
+#else
+ {.type=REVK_SETTINGS_UNSIGNED,.gpio=1,.name="blink",.comment="R, G, B LED array (set all the same for WS2812 LED)",.len=5,.dq=1,.def=quote(CONFIG_REVK_BLINK),.ptr=&blink,.size=sizeof(revk_gpio_t),.fix=1,.set=1,.flags="- ~↓↕⇕",.revk=1,.array=3},
+#endif
 #endif
 #ifdef  CONFIG_REVK_APMODE
 #ifdef	CONFIG_REVK_APCONFIG
@@ -68,7 +81,7 @@ revk_settings_t const revk_settings[]={
  {.type=REVK_SETTINGS_BLOB,.name="clientkey",.comment="Client Key (OTA and MQTT TLS)",.group=6,.len=9,.dot=6,.ptr=&clientkey,.malloc=1,.revk=1,.base64=1},
  {.type=REVK_SETTINGS_BLOB,.name="clientcert",.comment="Client certificate (OTA and MQTT TLS)",.group=6,.len=10,.dot=6,.ptr=&clientcert,.malloc=1,.revk=1,.base64=1},
 #if     defined(CONFIG_REVK_WIFI) || defined(CONFIG_REVK_MESH)
- {.type=REVK_SETTINGS_UNSIGNED,.name="wifireset",.comment="Restart if WiFi off for this long (seconds)",.group=7,.len=9,.dot=4,.dq=1,.def=quote(CONFIG_REVK_WIFIRESET),.ptr=&wifireset,.size=sizeof(uint16_t),.revk=1,.hide=1},
+ {.type=REVK_SETTINGS_UNSIGNED,.name="wifireset",.comment="Restart if WiFi off for this long (seconds)",.group=7,.len=9,.dot=4,.dq=1,.def=quote(CONFIG_REVK_WIFIRESET),.ptr=&wifireset,.size=sizeof(uint16_t),.revk=1},
  {.type=REVK_SETTINGS_STRING,.name="wifissid",.comment="WiFI SSID (name)",.group=7,.len=8,.dot=4,.dq=1,.def=quote(CONFIG_REVK_WIFISSID),.ptr=&wifissid,.malloc=1,.revk=1,.hide=1},
  {.type=REVK_SETTINGS_STRING,.name="wifipass",.comment="WiFi password",.group=7,.len=8,.dot=4,.dq=1,.def=quote(CONFIG_REVK_WIFIPASS),.ptr=&wifipass,.malloc=1,.revk=1,.hide=1,.secret=1},
  {.type=REVK_SETTINGS_STRING,.name="wifiip",.comment="WiFi Fixed IP",.group=7,.len=6,.dot=4,.dq=1,.def=quote(CONFIG_REVK_WIFIIP),.ptr=&wifiip,.malloc=1,.revk=1},
@@ -265,15 +278,29 @@ revk_settings_t const revk_settings[]={
 #ifdef	CONFIG_REVK_SETTINGS_PASSWORD
 char* password=NULL;
 #endif
+#ifdef  CONFIG_MDNS_MAX_INTERFACES
 char* hostname=NULL;
+#else
+char* hostname=NULL;
+#endif
 char* appname=NULL;
 char* otahost=NULL;
 uint8_t otadays=0;
 uint16_t otastart=0;
+#ifdef	CONFIG_REVK_WEB_BETA
+#endif
 revk_settings_blob_t* otacert=NULL;
 char* ntphost=NULL;
 char* tz=NULL;
 uint32_t watchdogtime=0;
+#ifdef  CONFIG_IDF_TARGET_ESP32S3
+uint16_t usbuptime=0;
+#endif
+#ifdef  CONFIG_IDF_TARGET_ESP32S3
+revk_gpio_t factorygpio={0};
+#else
+revk_gpio_t factorygpio={0};
+#endif
 char* topicgroup[2]={0};
 char* topiccommand=NULL;
 char* topicsetting=NULL;
@@ -283,12 +310,11 @@ char* topicinfo=NULL;
 char* topicerror=NULL;
 char* topicha=NULL;
 #ifdef	CONFIG_REVK_BLINK_DEF
+#ifdef	CONFIG_REVK_BLINK_WS2812_DEF
+revk_gpio_t blink={0};
+#else
 revk_gpio_t blink[3]={0};
 #endif
-#ifdef  CONFIG_IDF_TARGET_ESP32S3
-revk_gpio_t factorygpio={0};
-#else
-revk_gpio_t factorygpio={0};
 #endif
 #ifdef  CONFIG_REVK_APMODE
 #ifdef	CONFIG_REVK_APCONFIG
--- ESP32/main/settings.h
+++ ESP32/main/settings.h
@@ -40,8 +40,8 @@ struct revk_settings_s {
  uint8_t base64:1;
  uint8_t secret:1;
  uint8_t dq:1;
- uint8_t gpio:1;
  uint8_t rtc:1;
+ uint8_t gpio:1;
 };
 typedef struct revk_settings_blob_s revk_settings_blob_t;
 struct revk_settings_blob_s {
@@ -61,15 +61,25 @@ struct revk_gpio_s {
 enum {
 #ifdef	CONFIG_REVK_SETTINGS_PASSWORD
 #endif
+#ifdef  CONFIG_MDNS_MAX_INTERFACES
+#else
+#endif
  REVK_SETTINGS_BITFIELD_otaauto,
+#ifdef	CONFIG_REVK_WEB_BETA
  REVK_SETTINGS_BITFIELD_otabeta,
- REVK_SETTINGS_BITFIELD_prefixapp,
- REVK_SETTINGS_BITFIELD_prefixhost,
-#ifdef	CONFIG_REVK_BLINK_DEF
 #endif
  REVK_SETTINGS_BITFIELD_dark,
 #ifdef  CONFIG_IDF_TARGET_ESP32S3
+#endif
+#ifdef  CONFIG_IDF_TARGET_ESP32S3
 #else
+#endif
+ REVK_SETTINGS_BITFIELD_prefixapp,
+ REVK_SETTINGS_BITFIELD_prefixhost,
+#ifdef	CONFIG_REVK_BLINK_DEF
+#ifdef	CONFIG_REVK_BLINK_WS2812_DEF
+#else
+#endif
 #endif
 #ifdef  CONFIG_REVK_APMODE
 #ifdef	CONFIG_REVK_APCONFIG
@@ -124,15 +134,25 @@ typedef struct revk_settings_bits_s revk_settings_bits_t;
 struct revk_settings_bits_s {
 #ifdef	CONFIG_REVK_SETTINGS_PASSWORD
 #endif
+#ifdef  CONFIG_MDNS_MAX_INTERFACES
+#else
+#endif
  uint8_t otaauto:1;	// OTA auto upgrade
+#ifdef	CONFIG_REVK_WEB_BETA
  uint8_t otabeta:1;	// OTA from beta release
- uint8_t prefixapp:1;	// MQTT use appname/ in front of hostname in topic
- uint8_t prefixhost:1;	// MQTT use (appname/)hostname/topic instead of topic/(appname/)hostname
-#ifdef	CONFIG_REVK_BLINK_DEF
 #endif
  uint8_t dark:1;	// Default LED off
 #ifdef  CONFIG_IDF_TARGET_ESP32S3
+#endif
+#ifdef  CONFIG_IDF_TARGET_ESP32S3
 #else
+#endif
+ uint8_t prefixapp:1;	// MQTT use appname/ in front of hostname in topic
+ uint8_t prefixhost:1;	// MQTT use (appname/)hostname/topic instead of topic/(appname/)hostname
+#ifdef	CONFIG_REVK_BLINK_DEF
+#ifdef	CONFIG_REVK_BLINK_WS2812_DEF
+#else
+#endif
 #endif
 #ifdef  CONFIG_REVK_APMODE
 #ifdef	CONFIG_REVK_APCONFIG
@@ -184,19 +204,34 @@ struct revk_settings_bits_s {
 #define	MAX_MIX		5
 };
 #ifdef	CONFIG_REVK_SETTINGS_PASSWORD
-extern char* password;	// Settings password (this is not sent securely so use with care on local networks you control)
+extern char* password;	// Settings password<br>(not sent securely so use with care)
 #endif
-extern char* hostname;	// Host name
+#ifdef  CONFIG_MDNS_MAX_INTERFACES
+extern char* hostname;	// Hostname[.local]<br>(used in DHCP and MQTT)
+#else
+extern char* hostname;	// Host name<br>(used in DHCP and MQTT)
+#endif
 extern char* appname;	// Application name
 extern char* otahost;	// OTA hostname
 extern uint8_t otadays;	// OTA auto load (days)
 extern uint16_t otastart;	// OTA check after startup (min seconds)
 #define	otaauto	revk_settings_bits.otaauto
+#ifdef	CONFIG_REVK_WEB_BETA
 #define	otabeta	revk_settings_bits.otabeta
+#endif
 extern revk_settings_blob_t* otacert;	// OTA cert of otahost
+#define	dark	revk_settings_bits.dark
 extern char* ntphost;	// NTP host
 extern char* tz;	// Timezone (<a href='https://gist.github.com/alwynallan/24d96091655391107939' target=_blank>info</a>)
 extern uint32_t watchdogtime;	// Watchdog (seconds)
+#ifdef  CONFIG_IDF_TARGET_ESP32S3
+extern uint16_t usbuptime;	// USB  turns off after this many seconds
+#endif
+#ifdef  CONFIG_IDF_TARGET_ESP32S3
+extern revk_gpio_t factorygpio;	// Factory reset GPIO (press 3 times)
+#else
+extern revk_gpio_t factorygpio;	// Factory reset GPIO (press 3 times)
+#endif
 extern char* topicgroup[2];	// MQTT Alternative hostname accepted for commands
 extern char* topiccommand;	// MQTT Topic for commands
 extern char* topicsetting;	// MQTT Topic for settings
@@ -208,13 +243,11 @@ extern char* topicha;	// MQTT Topic for homeassistant
 #define	prefixapp	revk_settings_bits.prefixapp
 #define	prefixhost	revk_settings_bits.prefixhost
 #ifdef	CONFIG_REVK_BLINK_DEF
+#ifdef	CONFIG_REVK_BLINK_WS2812_DEF
+extern revk_gpio_t blink;	// WS2812 LED
+#else
 extern revk_gpio_t blink[3];	// R, G, B LED array (set all the same for WS2812 LED)
 #endif
-#define	dark	revk_settings_bits.dark
-#ifdef  CONFIG_IDF_TARGET_ESP32S3
-extern revk_gpio_t factorygpio;	// Factory reset GPIO (press 3 times)
-#else
-extern revk_gpio_t factorygpio;	// Factory reset GPIO (press 3 times)
 #endif
 #ifdef  CONFIG_REVK_APMODE
 #ifdef	CONFIG_REVK_APCONFIG
@@ -434,6 +467,261 @@ enum {
  REVK_SETTINGS_STRING,
  REVK_SETTINGS_OCTET,
 };
+#ifdef	CONFIG_REVK_SETTINGS_PASSWORD
+#define REVK_SETTINGS_PASSWORD
+#endif
+#ifdef  CONFIG_MDNS_MAX_INTERFACES
+#define REVK_SETTINGS_HOSTNAME
+#else
+#define REVK_SETTINGS_HOSTNAME
+#endif
+#define REVK_SETTINGS_APPNAME
+#define REVK_SETTINGS_OTAHOST
+#define REVK_SETTINGS_OTADAYS
+#define REVK_SETTINGS_OTASTART
+#define REVK_SETTINGS_OTAAUTO
+#ifdef	CONFIG_REVK_WEB_BETA
+#define REVK_SETTINGS_OTABETA
+#endif
+#define REVK_SETTINGS_OTACERT
+#define REVK_SETTINGS_DARK
+#define REVK_SETTINGS_NTPHOST
+#define REVK_SETTINGS_TZ
+#define REVK_SETTINGS_WATCHDOGTIME
+#ifdef  CONFIG_IDF_TARGET_ESP32S3
+#define REVK_SETTINGS_USBUPTIME
+#endif
+#ifdef  CONFIG_IDF_TARGET_ESP32S3
+#define REVK_SETTINGS_FACTORYGPIO
+#else
+#define REVK_SETTINGS_FACTORYGPIO
+#endif
+#define REVK_SETTINGS_TOPICGROUP
+#define REVK_SETTINGS_TOPICCOMMAND
+#define REVK_SETTINGS_TOPICSETTING
+#define REVK_SETTINGS_TOPICSTATE
+#define REVK_SETTINGS_TOPICEVENT
+#define REVK_SETTINGS_TOPICINFO
+#define REVK_SETTINGS_TOPICERROR
+#define REVK_SETTINGS_TOPICHA
+#define REVK_SETTINGS_PREFIXAPP
+#define REVK_SETTINGS_PREFIXHOST
+#ifdef	CONFIG_REVK_BLINK_DEF
+#ifdef	CONFIG_REVK_BLINK_WS2812_DEF
+#define REVK_SETTINGS_BLINK
+#else
+#define REVK_SETTINGS_BLINK
+#endif
+#endif
+#ifdef  CONFIG_REVK_APMODE
+#ifdef	CONFIG_REVK_APCONFIG
+#define REVK_SETTINGS_APPORT
+#endif
+#define REVK_SETTINGS_APTIME
+#define REVK_SETTINGS_APWAIT
+#define REVK_SETTINGS_APGPIO
+#endif
+#ifdef  CONFIG_REVK_MQTT
+#define REVK_SETTINGS_MQTTHOST
+#define REVK_SETTINGS_MQTTPORT
+#define REVK_SETTINGS_MQTTUSER
+#define REVK_SETTINGS_MQTTPASS
+#define REVK_SETTINGS_MQTTCERT
+#endif
+#define REVK_SETTINGS_CLIENTKEY
+#define REVK_SETTINGS_CLIENTCERT
+#if     defined(CONFIG_REVK_WIFI) || defined(CONFIG_REVK_MESH)
+#define REVK_SETTINGS_WIFIRESET
+#define REVK_SETTINGS_WIFISSID
+#define REVK_SETTINGS_WIFIPASS
+#define REVK_SETTINGS_WIFIIP
+#define REVK_SETTINGS_WIFIGW
+#define REVK_SETTINGS_WIFIDNS
+#define REVK_SETTINGS_WIFIBSSID
+#define REVK_SETTINGS_WIFICHAN
+#define REVK_SETTINGS_WIFIUPTIME
+#define REVK_SETTINGS_WIFIPS
+#define REVK_SETTINGS_WIFIMAXPS
+#endif
+#ifndef	CONFIG_REVK_MESH
+#define REVK_SETTINGS_APSSID
+#define REVK_SETTINGS_APPASS
+#define REVK_SETTINGS_APMAX
+#define REVK_SETTINGS_APIP
+#define REVK_SETTINGS_APLR
+#define REVK_SETTINGS_APHIDE
+#endif
+#ifdef	CONFIG_REVK_MESH
+#define REVK_SETTINGS_NODENAME
+#define REVK_SETTINGS_MESHRESET
+#define REVK_SETTINGS_MESHID
+#define REVK_SETTINGS_MESHKEY
+#define REVK_SETTINGS_MESHWIDTH
+#define REVK_SETTINGS_MESHDEPTH
+#define REVK_SETTINGS_MESHMAX
+#define REVK_SETTINGS_MESHPASS
+#define REVK_SETTINGS_MESHLR
+#define REVK_SETTINGS_MESHROOT
+#endif
+#define	AREAS	"ABCDEFGHIJKLMNOPQRSTUVWXYZZZZZZZ"
+#define REVK_SETTINGS_TAMPER
+#define REVK_SETTINGS_DEBUG
+#define REVK_SETTINGS_IOTSTATEDOOR
+#define REVK_SETTINGS_IOTSTATEINPUT
+#define REVK_SETTINGS_IOTSTATEOUTPUT
+#define REVK_SETTINGS_IOTSTATESYSTEM
+#define REVK_SETTINGS_IOTEVENTFOB
+#define REVK_SETTINGS_IOTEVENTARM
+#define REVK_SETTINGS_IOTKEYPAD
+#define REVK_SETTINGS_IOTGPS
+#define REVK_SETTINGS_IOTTOPIC
+#define REVK_SETTINGS_NFCRED
+#define REVK_SETTINGS_NFCAMBER
+#define REVK_SETTINGS_NFCGREEN
+#define REVK_SETTINGS_NFCCARD
+#define REVK_SETTINGS_NFCTX
+#define REVK_SETTINGS_NFCRX
+#define REVK_SETTINGS_NFCPOWER
+#define REVK_SETTINGS_NFCPOLL
+#define REVK_SETTINGS_NFCHOLDPOLL
+#define REVK_SETTINGS_NFCHOLD
+#define REVK_SETTINGS_NFCLONGHOLD
+#define REVK_SETTINGS_NFCLEDPOLL
+#define REVK_SETTINGS_NFCIOPOLL
+#define REVK_SETTINGS_NFCUART
+#define REVK_SETTINGS_NFCBAUD
+#define REVK_SETTINGS_NFCMQTTBELL
+#define REVK_SETTINGS_AES
+#define REVK_SETTINGS_AID
+#define REVK_SETTINGS_LEDIDLE
+#define REVK_SETTINGS_DOORAUTO
+#define REVK_SETTINGS_DOORUNLOCK
+#define REVK_SETTINGS_DOORLOCK
+#define REVK_SETTINGS_DOOROPEN
+#define REVK_SETTINGS_DOORCLOSE
+#define REVK_SETTINGS_DOORPROP
+#define REVK_SETTINGS_DOOREXIT
+#define REVK_SETTINGS_DOORPOLL
+#define REVK_SETTINGS_DOORDEBOUNCE
+#define REVK_SETTINGS_DOORDEBUG
+#define REVK_SETTINGS_DOOREXITARM
+#define REVK_SETTINGS_DOOREXITDISARM
+#define REVK_SETTINGS_DOORCATCH
+#define REVK_SETTINGS_FALLBACK
+#define REVK_SETTINGS_BLACKLIST
+#define REVK_SETTINGS_DOORIOTOPEN
+#define REVK_SETTINGS_DOORIOTDEAD
+#define REVK_SETTINGS_DOORIOTUNDEAD
+#define REVK_SETTINGS_DOORIOTLOCK
+#define REVK_SETTINGS_DOORIOTUNLOCK
+#define REVK_SETTINGS_GPSTX
+#define REVK_SETTINGS_GPSRX
+#define REVK_SETTINGS_GPSTICK
+#define REVK_SETTINGS_GPSUART
+#define	MAXOUTPUT	10
+#define	OUTPUT_FUNCS	"LDBE----"
+#define	OUTPUT_FUNC_L	0x80	// Lock release
+#define	OUTPUT_FUNC_D	0x40	// Deadlock release (this needs to be L>>1)
+#define	OUTPUT_FUNC_B	0x20	// Beep
+#define	OUTPUT_FUNC_E	0x10	// Error
+#define REVK_SETTINGS_BLINK
+#define REVK_SETTINGS_RGBS
+#define REVK_SETTINGS_OUTGPIO
+#define REVK_SETTINGS_OUTFUNC
+#define REVK_SETTINGS_OUTPULSE
+#define REVK_SETTINGS_OUTRGB
+#define REVK_SETTINGS_OUTNAME
+#define REVK_SETTINGS_OUTENGINEER
+#define REVK_SETTINGS_OUTFAULTED
+#define REVK_SETTINGS_OUTTAMPERED
+#define REVK_SETTINGS_OUTALARMED
+#define REVK_SETTINGS_OUTARMED
+#define REVK_SETTINGS_OUTPREARM
+#define REVK_SETTINGS_OUTPREALARM
+#define REVK_SETTINGS_OUTALARM
+#define REVK_SETTINGS_OUTDOORBELL
+#define REVK_SETTINGS_OUTWARNING
+#define REVK_SETTINGS_OUTFAULT
+#define REVK_SETTINGS_OUTACCESS
+#define REVK_SETTINGS_OUTPRESENCE
+#define REVK_SETTINGS_OUTTAMPER
+#define REVK_SETTINGS_OUTFIRE
+#define REVK_SETTINGS_OUTPANIC
+#define REVK_SETTINGS_POWERGPIO
+#define REVK_SETTINGS_POWERRGB
+#define	MAXINPUT	16
+#define	INPUT_FUNCS	"EOLDMP--"
+#define	INPUT_FUNC_E	0x80		// Exit pressed
+#define	INPUT_FUNC_O	0x40		// Open door
+#define	INPUT_FUNC_L	0x20		// Lock unlocked
+#define	INPUT_FUNC_D	0x10		// Deadlock unlocked (This needs to be L>>1)
+#define	INPUT_FUNC_M	0x08		// Manual active
+#define	INPUT_FUNC_P	0x04		// Pull down (not really a function, but a rarely needed input flag)
+#define REVK_SETTINGS_INPOLL
+#define REVK_SETTINGS_INGPIO
+#define REVK_SETTINGS_INRGB
+#define REVK_SETTINGS_INFUNC
+#define REVK_SETTINGS_INHOLD
+#define REVK_SETTINGS_INNAME
+#define REVK_SETTINGS_INDOORBELL
+#define REVK_SETTINGS_INWARNING
+#define REVK_SETTINGS_INFAULT
+#define REVK_SETTINGS_INACCESS
+#define REVK_SETTINGS_INPRESENCE
+#define REVK_SETTINGS_INTAMPER
+#define REVK_SETTINGS_INFIRE
+#define REVK_SETTINGS_INPANIC
+#define REVK_SETTINGS_INARM
+#define REVK_SETTINGS_INSTRONG
+#define REVK_SETTINGS_INDISARM
+#define REVK_SETTINGS_KEYPADTX
+#define REVK_SETTINGS_KEYPADRX
+#define REVK_SETTINGS_KEYPADDE
+#define REVK_SETTINGS_KEYPADRE
+#define REVK_SETTINGS_KEYPADCLK
+#define REVK_SETTINGS_KEYPADTIMER
+#define REVK_SETTINGS_KEYPADADDRESS
+#define REVK_SETTINGS_KEYPADTXPRE
+#define REVK_SETTINGS_KEYPADTXPOST
+#define REVK_SETTINGS_KEYPADRXPRE
+#define REVK_SETTINGS_KEYPADRXPOST
+#define REVK_SETTINGS_KEYPADIDLE
+#define REVK_SETTINGS_KEYPADPIN
+#define REVK_SETTINGS_KEYPADPINARM
+#define	MAX_MIX		5
+#define REVK_SETTINGS_AREALED
+#define REVK_SETTINGS_AREAENTER
+#define REVK_SETTINGS_AREAARM
+#define REVK_SETTINGS_AREASTRONG
+#define REVK_SETTINGS_AREADISARM
+#define REVK_SETTINGS_AREADEADLOCK
+#define REVK_SETTINGS_AREAKEYPAD
+#define REVK_SETTINGS_AREAKEYDISARM
+#define REVK_SETTINGS_AREAKEYSTRONG
+#define REVK_SETTINGS_AREAKEYARM
+#define REVK_SETTINGS_ENGINEER
+#define REVK_SETTINGS_ARMED
+#define REVK_SETTINGS_SMSARM
+#define REVK_SETTINGS_SMSDISARM
+#define REVK_SETTINGS_SMSCANCEL
+#define REVK_SETTINGS_SMSARMFAIL
+#define REVK_SETTINGS_SMSALARM
+#define REVK_SETTINGS_SMSPANIC
+#define REVK_SETTINGS_SMSFIRE
+#define REVK_SETTINGS_MIXAND
+#define REVK_SETTINGS_MIXSET
+#define REVK_SETTINGS_ARMCANCEL
+#define REVK_SETTINGS_ARMDELAY
+#define REVK_SETTINGS_ALARMDELAY
+#define REVK_SETTINGS_ALARMHOLD
+#define REVK_SETTINGS_MESHEXPECT
+#define REVK_SETTINGS_MESHCYCLE
+#define REVK_SETTINGS_MESHWARMUP
+#define REVK_SETTINGS_MESHFLAP
+#define REVK_SETTINGS_MESHDIED
+#define REVK_SETTINGS_MQTTDIED
+#define REVK_SETTINGS_SMSNUMBER
+#define REVK_SETTINGS_TIMER1
 #define	REVK_SETTINGS_HAS_OLD
 #define	REVK_SETTINGS_HAS_COMMENT
 #define	REVK_SETTINGS_HAS_GPIO
@@ -444,6 +732,6 @@ enum {
 #define	REVK_SETTINGS_HAS_BLOB
 #define	REVK_SETTINGS_HAS_STRING
 #define	REVK_SETTINGS_HAS_OCTET
-typedef uint8_t revk_setting_bits_t[26];
+typedef uint8_t revk_setting_bits_t[27];
 typedef uint8_t revk_setting_group_t[3];
 extern const char revk_settings_secret[];
